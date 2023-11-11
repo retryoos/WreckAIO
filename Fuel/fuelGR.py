75fa4abc10e4f4791d0e56601e9f076ebb9c50f4
Dimitris <dimitriskalligaridis@gmail.com>
2023-11-11 12:23:18 +0200

    Update fuelGR.py

--- Fuel/fuelGR.py
+++ Fuel/fuelGR.py
@@ -5,24 +5,26 @@ class FuelGr():
         os.system("title WreckAIO - FuelGR")
         print("Starting FuelGR")
         time.sleep(5)
-    
-    
-'''    
-sizes = {
-        "40.5":"5731",
-        "41":"5860",
-        "42":"5732",
-        "42.5":"5862",
-        "43":"5733",
-        "44.5":"5735",
-        "45.5":"5736",
-        "47.5":"5869",
-        "48.5":"5870",
-        "50":"5872",
-    }
-    size = sizes[size]
 
-    s = requests.Session()
+    # Did not manage to make an auto-checkout script for Fuel
+
+    '''    
+    sizes = {
+            "40.5":"5731",
+            "41":"5860",
+            "42":"5732",
+            "42.5":"5862",
+            "43":"5733",
+            "44.5":"5735",
+            "45.5":"5736",
+            "47.5":"5869",
+            "48.5":"5870",
+            "50":"5872",
+        }
+        size = sizes[size]
+
+        s = requests.Session()
     
-    checkout = s.get("https://www.fuel.com.gr/el/checkout/")
-    form_action = page.find("form", id="product_addtocart_form").get("action")'''
+        checkout = s.get("https://www.fuel.com.gr/el/checkout/")
+        form_action = page.find("form", id="product_addtocart_form").get("action")
+    '''
